9b644119e22f723309b5025343ce4768f5803e4d
annprog <tecbbs@qq.com>
2019-04-12 19:10:33 +0800

    beamer support classyslides

--- templates/beamerthemes/classyslides/patch-classyslides.sh
+++ templates/beamerthemes/classyslides/patch-classyslides.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+getEnv COLORTHEME "dark"
+THEMEOPT="$THEMEOPT -V themeoptions=colors=$COLORTHEME"
 customHeader="--template=classyslides.tpl"
+note "THEMEOPT: $THEMEOPT"
+sed -i '/shadowbox/d' listings-set.tex
