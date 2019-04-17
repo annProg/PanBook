fe9d49f835d1edd6ad0e39df01c25648f2fd3d68
annprog <tecbbs@qq.com>
2019-04-17 19:55:00 +0800

    fix elegantbook conflict with unicode-math. add elegantnote

--- templates/pdfclasses/elegantbook/patch-elegantbook.sh
+++ templates/pdfclasses/elegantbook/patch-elegantbook.sh
@@ -5,8 +5,14 @@
 note "use -E device=(pc|mobile|kindel) to produce different size of pdf"
 note "use -E cover=path to use custom cover image"
 note "use -E logo=path to use custom logo image"
+note "use -E privatetpl=(true|false) to use custom template define by this class"
 
-addOptions="--template=elegantbook.tpl -V device=$device"
+getVar privatetpl "true"
+[ "$privatetpl"x == "true"x ] && addOptions="--template=elegantbook.tpl"
+
+# 默认模板需要禁用mathspec. unicode-math和newtxmath不兼容
+# https://github.com/ElegantLaTeX/ElegantBook/issues/9
+addOptions="$addOptions -V device=$device -V mathspec=false"
 division="--top-level-division=chapter"
 highLight=""
 
