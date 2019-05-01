54cf97593b19417bfcc53e3d8a295f79cb40d17c
annProg <tecbbs@qq.com>
2019-05-01 21:02:48 +0800

    [test] add test dot code to image convert

---
+++ test/lua-filters/dot2image/dot2image.lua
@@ -0,0 +1,27 @@
+-- Pandoc filter to process code blocks with class "ci-dot" containing
+-- graphviz dot code into images.
+--
+-- * Assumes that dot in the path.
+-- * For textual output formats, use --extract-media=dot-images
+-- * For HTML formats, you may alternatively use --self-contained
+
+local filetypes = { html = {"png", "image/png"}
+                  , latex = {"pdf", "application/pdf"}
+                  }
+local filetype = filetypes[FORMAT][1] or "png"
+local mimetype = filetypes[FORMAT][2] or "image/png"
+
+local function dot2image(dot, filetype)
+    local image = pandoc.pipe("dot", {"-T" .. filetype}, dot)
+    -- local final = pandoc.pipe("convert", {"-", filetype .. ":-"}, eps)
+    return image
+end
+
+function CodeBlock(block)
+    if block.classes[1] == "ci-dot" then
+        local img = dot2image(block.text, filetype)
+        local fname = pandoc.sha1(img) .. "." .. filetype
+        pandoc.mediabag.insert(fname, mimetype, img)
+        return pandoc.Para{ pandoc.Image({pandoc.Str("dot tune")}, fname) }
+    end
+end
---
+++ test/lua-filters/dot2image/test.md
@@ -0,0 +1,5 @@
+# Dot code to Image Test
+
+```ci-dot
+digraph G { a->b;}
+```
