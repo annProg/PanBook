d64e122fa67af5ea3bc892e5a205339dfa18c009
annProg <tecbbs@qq.com>
2019-05-03 22:43:08 +0800

    fix patch-moderncv.sh

--- templates/cvtpls/moderncv/patch-moderncv.sh
+++ templates/cvtpls/moderncv/patch-moderncv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-note "use -E style=(classic|casul|oldstyle|banking|empty) default classic"
-note "use -E color=(blue|orange|green|red|purple|grey|black|burgundy) default blue"
-note "use -E fontsize=(10pt|11pt|12pt) default 11pt"
-note "use -E size=(a4paper|letterpaper|a5paper|legalpaper|executivepaper|landscape) default a4paper"
-note "use -E fontfamily=(roman|sans) default sans"
+note "use -V style=(classic|casul|oldstyle|banking|empty) default classic"
+note "use -V color=(blue|orange|green|red|purple|grey|black|burgundy) default blue"
+note "use -V fontsize=(10pt|11pt|12pt) default 11pt"
+note "use -V size=(a4paper|letterpaper|a5paper|legalpaper|executivepaper|landscape) default a4paper"
+note "use -V fontfamily=(roman|sans) default sans"
 
 setPandocVar style "classic"
 setPandocVar color "blue"
