0dd2755414e53ec4c00bad3d3d230a9b4a2356de
annProg <tecbbs@qq.com>
2019-05-13 01:24:28 +0800

    [cv] add default makefile

--- libs/module-cv.sh
+++ libs/module-cv.sh
@@ -5,6 +5,7 @@ function cvMeta() {
 	cvFile=$WORKDIR/cv.md
 	photoFile=$IMGDIR/photo.png
 	bibFile=$WORKDIR/bibliography.bib
+	makeFile=$cwd/Makefile
 	[ ! -f $photoFile ] && cp $SCRIPTDIR/medias/photo.png $photoFile
 	[ ! -f $cvFile ] && cat > $cvFile <<EOF
 ---
@@ -99,6 +100,30 @@ EOF
 	journal={panbook},
 	year={2019}
 }	
+EOF
+
+	[ ! -f $makeFile ] && cat > $makeFile <<EOF
+STYLE ?= classic
+COLOR ?= burgundy
+ALL_STYLE=classic banking casual oldstyle fancy
+ALL_COLOR=blue orange green red purple grey black burgundy
+all: moderncv-styles resume
+moderncv:
+	panbook cv -E style=$(STYLE) -E color=$(COLOR)
+moderncv-styles:
+        for style in `echo $(ALL_STYLE)`;do \
+                panbook cv -E style=$$style -E color=$(COLOR) -V onlinecv="" -V showdate=""; \
+        done
+moderncv-all:
+        for style in `echo $(ALL_STYLE)`;do \
+                for color in `echo $(ALL_COLOR)`;do \
+                        panbook cv -E style=$$style -E color=$$color; \
+                done \
+        done
+resume:
+        panbook cv --cv=resume
+all-tpl:
+        panbook cv --cv=A		
 EOF
 	
 }
