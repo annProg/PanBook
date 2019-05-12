65cbed81410b19be6d6b34b131084d2712c37ff6
annProg <tecbbs@qq.com>
2019-05-13 01:34:42 +0800

    [cv] fix cv default Makefile

--- libs/module-cv.sh
+++ libs/module-cv.sh
@@ -7,6 +7,7 @@ function cvMeta() {
 	bibFile=$WORKDIR/bibliography.bib
 	makeFile=$cwd/Makefile
 	[ ! -f $photoFile ] && cp $SCRIPTDIR/medias/photo.png $photoFile
+	[ ! -f $makeFile ] && cp $SCRIPTDIR/libs/scripts/cv.Makefile $makeFile
 	[ ! -f $cvFile ] && cat > $cvFile <<EOF
 ---
 name: 姓\ 名        # use backslash to escape space
@@ -100,30 +101,6 @@ EOF
 	journal={panbook},
 	year={2019}
 }	
-EOF
-
-	[ ! -f $makeFile ] && cat > $makeFile <<EOF
-STYLE ?= classic
-COLOR ?= burgundy
-ALL_STYLE=classic banking casual oldstyle fancy
-ALL_COLOR=blue orange green red purple grey black burgundy
-all: moderncv-styles resume
-moderncv:
-	panbook cv -E style=$(STYLE) -E color=$(COLOR)
-moderncv-styles:
-        for style in `echo $(ALL_STYLE)`;do \
-                panbook cv -E style=$$style -E color=$(COLOR) -V onlinecv="" -V showdate=""; \
-        done
-moderncv-all:
-        for style in `echo $(ALL_STYLE)`;do \
-                for color in `echo $(ALL_COLOR)`;do \
-                        panbook cv -E style=$$style -E color=$$color; \
-                done \
-        done
-resume:
-        panbook cv --cv=resume
-all-tpl:
-        panbook cv --cv=A		
 EOF
 	
 }
