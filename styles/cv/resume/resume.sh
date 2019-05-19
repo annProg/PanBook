af1a8f3ef7a873d5c88253e6c47e9bc93241babf
annProg <tecbbs@qq.com>
2019-05-19 17:11:40 +0800

    fix resume

---
+++ styles/cv/resume/resume.sh
@@ -0,0 +1,28 @@
+function func_cv_resume_help() {
+note "This Template support the following variables, they can set via metadata or -V option\n
+\tname                 Your name
+\ttitle                Your title
+\tmobile               Your mobile phone number
+\tfixed                Your fixed phone number
+\tgithub               Your Github user name
+\tlinkedin             Your LinkedIn account
+\ttwitter              Your Twitter account
+\twechat               Your WeChat account
+\tweibo                Your Weibo account
+\tqq                   Your QQ number
+\tskype                Your Skype
+\thomepage             Your website link
+\temail                Your Email address
+\taddress              Your address
+\textrainfo            Additional information
+\tphoto                Your photo(put photo to src/images/ dir; photo value should be images/filename)
+\tquote                Some quote
+\tonlinecv             Your cv url
+\tshowdate             Show cv compile date (true|false)
+\tCJKmainfont          Change CJKmainfont. default SimSun
+\tCJKoptions           Change CJKoptions default (BoldFont=微软雅黑,ItalicFont=KaiTi,SmallCapsFont=微软雅黑)"
+}
+
+func_cv_resume_list() {
+    note "cv style resume no list"
+}
