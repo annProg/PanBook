f07a0ca423144da7de191e96f19fb03e78e32a41
annProg <tecbbs@qq.com>
2019-05-19 17:15:46 +0800

    fix ext column

--- extensions/column/column.sh
+++ extensions/column/column.sh
@@ -2,7 +2,7 @@ regext column
 getArrayVar _G "ext-column" true
 
 function ext_column() {
-	if [ "${_G[ext-column-on]}"x == "true"x ];then
+	if [ "${_G[ext-column]}"x == "true"x ];then
 		_F[column]="--lua-filter $SCRIPTDIR/${_G[extdir]}/column/fenced_divs_columns.lua"
 	fi
 }
