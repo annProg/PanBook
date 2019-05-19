b4f5e75e47464d8a9c8f3066a360da6787845686
annProg <tecbbs@qq.com>
2019-05-19 21:59:26 +0800

    fix slide.sh

--- modules/slide/slide.sh
+++ modules/slide/slide.sh
@@ -1,50 +1,57 @@
-function func_beamer() {
-	getVar TPL "latex"
-	getVar THEME "metropolis"
-	getVar DOCUMENTCLASS "ctexbeamer"
-	SLIDETPLDIR="slide"
-	
-	themeList=(solarized material HeavenlyClouds Xiaoshan Execushares classyslides Hest opensuse boxes CambridgeUS classic Dresden EastLansing lined Singapore cuerna   metropolis)	
-	
-	# 支持随机选取theme
-	setTheme "${themeList[*]}"
+# 注册模块及帮助信息
+regmod slide
+_H[slide]="make slide"
 
-	for t in ${SELECTED[@]};do
-		init
-		cd $BUILD
+function func_slide() {
+	initBody cv.md
+	initBib true
+	initMakefile
 
-		note "THEME: $t"
-		addOptions="$origAddOptions"
-		PANDOCVARS="$ORIGPANDOCVARS"
-		setPandocVar "classoption=aspectratio" "169" # setPandocVar需在PANDOCVARS被ORIGPANDOCVARS赋值之后执行
-		# copy beamertheme
-		BEAMERTHEMEDIR=$SCRIPTDIR/templates/$SLIDETPLDIR/$t
-		USERDEFINETHMEME=$cwd/templates/$SLIDETPLDIR/$t
-		if [ -d $BEAMERTHEMEDIR -o -d $USERDEFINETHMEME ];then
-			cp -rfu $BEAMERTHEMEDIR $BUILD 2>/dev/null
-			cp -rfu $USERDEFINETHMEME $BUILD 2>/dev/null
-			# 需要删除.md文件
-			rm -f $t/*.md
-			rm -f $t/*.pdf
-			cp -rfu $t/* .
-		fi
-		
-		PANDOCVARS="$PANDOCVARS -V theme=$t -V documentclass=$DOCUMENTCLASS"
-		# 某些theme需要打补丁. 补丁放在theme文件夹下，命名规则 patch-$themename.sh
-		[ -f patch-$t.sh ] && source patch-$t.sh
-		
-		[ "$LSTSET"x != ""x ] && (cat $LSTSET;echo) >> $HEADERS
-		info "PANDOCVARS: $PANDOCVARS"
-		info "addOptions: $addOptions"
-		info "LSTSET: $LSTSET"
-		
-		OUTPUT="$BUILD/$ofile-beamer-$t.pdf"
-		# output tex for debug
-		[ "$DEBUG"x == "true"x ] && \
-		pandoc $PANDOC_REFERENCE_PARAM --listings -t beamer $BODY -o $OUTPUT.tex --pdf-engine=xelatex $PANDOCVARS --metadata-file=$METADATA $addOptions
-		pandoc $PANDOC_REFERENCE_PARAM --listings -t beamer $BODY -o $OUTPUT --pdf-engine=xelatex $PANDOCVARS --metadata-file=$METADATA $addOptions
-		compileStatus beamer
-	done
-	
-	clean
+	getArrayVar _G style metropolis
+	getArrayVar _V documentclass ctexbeamer
+	getArrayVar _V classoption "aspectratio=169"
+	_V[theme]=${_G[style]}
+
+	STYLESLIDE=$SCRIPTDIR/${_G[styleslide]}
+	USERSTYLESLIDE=$CWD/${_G[styleslide]}
+
+	init
+	cd ${_G[build]}
+	t=${_G[style]}
+	note "style: $t"
+	# copy beamertheme
+	if [ -d $STYLESLIDE/$t -o -d $USERSTYLESLIDE/$t ];then
+		cp -rfu $STYLESLIDE/$t ${_G[build]} 2>/dev/null
+		cp -rfu $USERSTYLESLIDE/$t ${_G[build]} 2>/dev/null
+		# 需要删除.md文件
+		rm -f $t/*.md
+		rm -f $t/*.pdf
+		cp -rfu $t/* .
+	fi
+
+	_G[ofile]=${_G[build]}/${_G[ofile]}-${_G[function]}-${_G[style]}
+
+	# 某些theme需要打补丁. 补丁放在theme文件夹下，命名规则 patch-$themename.sh
+	[ -f patch-$t.sh ] && source patch-$t.sh
+
+	# 自动扫描风格目录下的lua filter
+	[ -f $t.lua ] && _F[style-${_G[function]}-$t]="--lua-filter $t.lua"
+	# 如有tpl，自动加载
+	[ -f $t.tpl ] && _P[template]="$t.tpl"
+
+	# 启用扩展
+	ext_header
+	ext_listings
+	ext_grade
+
+	if [ "${_G[debug]}"x != "true"x ];then
+		_G[t]=pdf
+	else
+		_P[standalone]=""
+	fi
+
+	_P[to]=beamer
+	getPandocParam
+	pandoc ${_G[pandoc-param]}
+	compileStatus ${_G[function]}
 }
