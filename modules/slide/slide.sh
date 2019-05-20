fe9bb707bf128d1ba3c09ae04004cba78c3b69ee
annprog <tecbbs@qq.com>
2019-05-20 18:29:39 +0800

    slide调用libs/functions.sh

--- modules/slide/slide.sh
+++ modules/slide/slide.sh
@@ -12,32 +12,9 @@ function func_slide() {
 	getArrayVar _V classoption "aspectratio=169"
 	_V[theme]=${_G[style]}
 
-	STYLESLIDE=$SCRIPTDIR/${_G[styleslide]}
-	USERSTYLESLIDE=$CWD/${_G[styleslide]}
-
 	init
-	cd ${_G[build]}
-	t=${_G[style]}
-	note "style: $t"
-	# copy beamertheme
-	if [ -d $STYLESLIDE/$t -o -d $USERSTYLESLIDE/$t ];then
-		cp -rfu $STYLESLIDE/$t ${_G[build]} 2>/dev/null
-		cp -rfu $USERSTYLESLIDE/$t ${_G[build]} 2>/dev/null
-		# 需要删除.md文件
-		rm -f $t/*.md
-		rm -f $t/*.pdf
-		cp -rfu $t/* .
-	fi
-
-	_G[ofile]=${_G[build]}/${_G[ofile]}-${_G[function]}-${_G[style]}
-
-	# 某些theme需要打补丁. 补丁放在theme文件夹下，命名规则 patch-$themename.sh
-	[ -f patch-$t.sh ] && source patch-$t.sh
-
-	# 自动扫描风格目录下的lua filter
-	[ -f $t.lua ] && _F[style-${_G[function]}-$t]="--lua-filter $t.lua"
-	# 如有tpl，自动加载
-	[ -f $t.tpl ] && _P[template]="$t.tpl"
+	_copystyle
+	_patch
 
 	# 启用扩展
 	ext_header
