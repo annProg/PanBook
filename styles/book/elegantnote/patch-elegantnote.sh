b4ba757967aea3533a6f873438e8e3a4323ba6b2
annProg <tecbbs@qq.com>
2019-05-26 15:00:09 +0800

    add elegantnote

--- styles/book/elegantnote/patch-elegantnote.sh
+++ styles/book/elegantnote/patch-elegantnote.sh
@@ -2,4 +2,6 @@
 
 # 默认模板需要禁用mathspec. unicode-math和newtxmath不兼容
 # https://github.com/ElegantLaTeX/ElegantBook/issues/9
-addOptions="$addOptions -V mathspec=false"
+getArrayVar _V mathspec "false"
+_V[documentclass]="elegantnote"
+_P[top-level-division]="section"
