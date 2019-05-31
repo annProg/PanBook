3ea6a41400523a787c2db41d032ea94edbbb12a6
annProg <tecbbs@qq.com>
2019-05-31 21:49:40 +0800

    fix note

--- extensions/note/note.lua
+++ extensions/note/note.lua
@@ -1,12 +1,18 @@
+--[[ only for debug
+table_print = require('table_print')
+table.print = table_print.print_r
+--]]
+
 function Div(el)
 	if el.attr.classes[1] == "note" then
-		ret = pandoc.Div({})
-		table.insert(ret.content, pandoc.RawInline("latex", "\\begin{note}"))
+		ret = pandoc.Para({})
+		table.insert(ret.content, pandoc.RawInline("latex", "\\begin{note}\n"))
 		for k,v in pairs(el.content) do
-			table.insert(ret.content, v)
+			for i,val in pairs(v.content) do
+				table.insert(ret.content, val)
+			end
 		end
-		table.insert(ret.content, pandoc.RawInline("latex", "\\end{note}"))
+		table.insert(ret.content, pandoc.RawInline("latex", "\n\\end{note}"))
 		return ret
 	end
-	return el
 end
