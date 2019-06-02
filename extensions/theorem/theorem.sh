e48c3b3ac46ceb48e9f577fa56e5804dd72f07b9
annProg <tecbbs@qq.com>
2019-06-02 20:34:23 +0800

    fix theorem reference

--- extensions/theorem/theorem.sh
+++ extensions/theorem/theorem.sh
@@ -9,9 +9,9 @@ function ext_theorem() {
 	getArrayVar _G "ext-theorem-texlang" "$SCRIPTDIR/${_G[extdir]}/theorem/${_V[lang]}.tex"
 	# 依赖add header功能
 	if [ "${_G[ext-theorem]}"x == "true"x -a "${_G[ext-header]}"x == "true"x ];then
+		writeHeader ${_G[ext-theorem-texlang]}
 		# 第一个参数为 tex 时才使用扩展种的theorem定义（有些文档类定义了theorem环境）
 		if [ "$1"x == "tex"x ];then
-			writeHeader ${_G[ext-theorem-texlang]}
 			writeHeader ${_G[ext-theorem-color]}
 			writeHeader ${_G[ext-theorem-tex]}
 		fi
