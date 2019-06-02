e60a80d65253fb2e0d7028ff4f00ae09a116ce6a
annProg <tecbbs@qq.com>
2019-06-02 11:06:44 +0800

    wrap -V lang

--- extensions/wrap/wrap.sh
+++ extensions/wrap/wrap.sh
@@ -1,12 +1,12 @@
 regext wrap
 getArrayVar _G "ext-wrap" true
 getArrayVar _G "ext-wrap-style" "bclogo"
-getArrayVar _G "ext-wrap-lang" "zh"
+getArrayVar _V "lang" "zh"
 getArrayVar _G "ext-wrap-color" $SCRIPTDIR/${_G[extdir]}/wrap/color.tex
 
 function ext_wrap() {
 	getArrayVar _G "ext-wrap-tex" "$SCRIPTDIR/${_G[extdir]}/wrap/${_G[ext-wrap-style]}-wrap.tex"
-	getArrayVar _G "ext-wrap-texlang" "$SCRIPTDIR/${_G[extdir]}/wrap/${_G[ext-wrap-lang]}.tex"
+	getArrayVar _G "ext-wrap-texlang" "$SCRIPTDIR/${_G[extdir]}/wrap/${_V[lang]}.tex"
 	# 依赖add header功能
 	if [ "${_G[ext-wrap]}"x == "true"x -a "${_G[ext-header]}"x == "true"x ];then
 		# 第一个参数为 tex 时才使用扩展种的wrap定义（有些文档类定义了wrap环境）
@@ -25,6 +25,6 @@ function ext_wrap_help() {
 	echo -e "\t-G ext-wrap-texlang:<custom language file>      change language file"
 	echo -e "\t-G ext-wrap-style:<bclogo|tcolorbox>            select wrap style"
 	echo -e "\t-G ext-wrap-color:<custom color set file>       use custom color set file"
-	echo -e "\t-G ext-wrap-lang:<zh|en>                        select language"
+	echo -e "\t-V lang:<zh|en>                                 select language"
 	exit 0
 }
