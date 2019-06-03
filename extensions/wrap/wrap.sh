1a1168149c98f014bdb274887c8ab8b17aa8f9da
annprog <tecbbs@qq.com>
2019-06-03 14:34:10 +0800

    ext wrap add introduction and problemset

--- extensions/wrap/wrap.sh
+++ extensions/wrap/wrap.sh
@@ -3,17 +3,27 @@ getArrayVar _G "ext-wrap" true
 getArrayVar _G "ext-wrap-style" "bclogo"
 getArrayVar _V "lang" "zh"
 getArrayVar _G "ext-wrap-color" $SCRIPTDIR/${_G[extdir]}/wrap/color.tex
+getArrayVar _G "ext-wrap-introduction" $SCRIPTDIR/${_G[extdir]}/wrap/introduction.tex
+getArrayVar _G "ext-wrap-problemset" $SCRIPTDIR/${_G[extdir]}/wrap/problemset.tex
 
 function ext_wrap() {
 	getArrayVar _G "ext-wrap-tex" "$SCRIPTDIR/${_G[extdir]}/wrap/${_G[ext-wrap-style]}-wrap.tex"
 	getArrayVar _G "ext-wrap-texlang" "$SCRIPTDIR/${_G[extdir]}/wrap/${_V[lang]}.tex"
 	# 依赖add header功能
 	if [ "${_G[ext-wrap]}"x == "true"x -a "${_G[ext-header]}"x == "true"x ];then
+		writeHeader ${_G[ext-wrap-texlang]}
 		# 第一个参数为 tex 时才使用扩展种的wrap定义（有些文档类定义了wrap环境）
 		if [ "$1"x == "tex"x ];then
 			writeHeader ${_G[ext-wrap-color]}
-			writeHeader ${_G[ext-wrap-texlang]}
 			writeHeader ${_G[ext-wrap-tex]}
+		fi
+		# 如果style本身支持introduction和problemset，可以unset _G[ext-wrap-introduction]
+		writeHeader ${_G[ext-wrap-introduction]}
+		writeHeader ${_G[ext-wrap-problemset]}
+
+		# noanswer功能,elegantbook文档类可以通过classoption设置，其他文档类通过_G[ext-wrap-noanswer]=true来设置
+		if [ "${_G[ext-wrap-noanswer]}"x == "true"x ];then
+			writeHeader $SCRIPTDIR/${_G[extdir]}/wrap/noanswer.tex
 		fi
 		_F[wrap]="--lua-filter $SCRIPTDIR/${_G[extdir]}/wrap/wrap.lua"
 	fi
@@ -25,6 +35,9 @@ function ext_wrap_help() {
 	echo -e "\t-G ext-wrap-texlang:<custom language file>      change language file"
 	echo -e "\t-G ext-wrap-style:<bclogo|tcolorbox>            select wrap style"
 	echo -e "\t-G ext-wrap-color:<custom color set file>       use custom color set file"
+	echo -e "\t-G ext-wrap-introduction:<custom introduction>  use custom introduction"
+	echo -e "\t-G ext-wrap-problemset:<custom problemset>      use custom problemset"
+	echo -e "\t-G ext-wrap-noanswer:<true|false>               noanswer mode"
 	echo -e "\t-V lang:<zh|en>                                 select language"
 	exit 0
 }
--- styles/book/elegantbook/patch-elegantbook.sh
+++ styles/book/elegantbook/patch-elegantbook.sh
@@ -8,6 +8,10 @@ note "use -V logo:path to use custom logo image"
 
 # 启用扩展
 ext_device
+
+# introduction和problemset文档类中已有定义，取消writeHeader
+unset _G[ext-wrap-introduction]
+unset _G[ext-wrap-problemset]
 ext_wrap tex
 ext_theorem
 # 默认模板需要禁用mathspec. unicode-math和newtxmath不兼容
