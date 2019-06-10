624aa2d4351c0e0479196e1d54b432dee428ed9c
annprog <tecbbs@qq.com>
2019-06-10 16:42:29 +0800

    fix test highlight

--- test/highlight/minted.lua
+++ test/highlight/minted.lua
@@ -3,6 +3,13 @@ table_print = require('table_print')
 table.print = table_print.print_r
 --]]
 
+function Meta(meta)
+	header = pandoc.MetaBlocks({})
+	table.insert(header, pandoc.RawBlock("latex", "\\usepackage{minted}"))
+	meta['header-includes'] = header
+	return meta
+end
+
 function CodeBlock(block)
 	id = block.attr.identifier
 	language = block.attr.classes[1]
@@ -12,7 +19,9 @@ function CodeBlock(block)
 	end
 
 	for k,v in pairs(block.attr.attributes) do
-		table.insert(option, k .. "={" .. v .. "}")
+		if k ~= 'caption' then
+			table.insert(option, k .. "={" .. v .. "}")
+		end
 	end
 
 	table.insert(option, "label=" .. id)
