4211d4dc24a39ff01593a832053605d25f21b54c
annprog <tecbbs@qq.com>
2019-06-10 17:35:58 +0800

    fix test highlight minted.lua

--- test/highlight/minted.lua
+++ test/highlight/minted.lua
@@ -16,22 +16,24 @@ function Meta(meta)
 end
 
 function CodeBlock(block)
-	id = block.attr.identifier
-	language = block.attr.classes[1]
-	option = {}
+	local id = block.attr.identifier
+	local language = block.attr.classes[1]
+	local option = {}
 	for i=2,#block.attr.classes do
 		table.insert(option, block.attr.classes[i])
 	end
 
+	local label = ""
+	if id ~= nil then
+		label = "\\hypertarget{" .. id .. "}{%\n\\label{" .. id .. "}}%\n"
+	end
 	for k,v in pairs(block.attr.attributes) do
 		if k ~= 'caption' then
 			table.insert(option, k .. "={" .. v .. "}")
 		end
 	end
 
-	table.insert(option, "label=" .. id)
-
-	optionStr = table.concat(option, ",")
-	minted = "\\begin{minted}[" .. optionStr .. "]{" .. language .. "}\n" .. block.text .. "\n\\end{minted}"
-	return pandoc.RawBlock("latex", minted)
+	local optionStr = table.concat(option, ",")
+	local minted = "\\begin{minted}[" .. optionStr .. "]{" .. language .. "}\n" .. block.text .. "\n\\end{minted}"
+	return pandoc.RawBlock("latex", label .. minted)
 end
