8bd49f671f52e6864aca161e3a5080b6282d44ec
annProg <tecbbs@qq.com>
2019-06-23 23:39:56 +0800

    fix slide style HeavenlyClouds. lishu字体问题

--- styles/slide/HeavenlyClouds/patch-HeavenlyClouds.sh
+++ styles/slide/HeavenlyClouds/patch-HeavenlyClouds.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-(cat fix-heavenlyclouds.tex;echo) >> ${_G[header]}
+uname -s |grep "_NT" &>/dev/null && os=windows || os=""
+
+if [ "$os"x == "windows"x ];then
+	(cat fix-heavenlyclouds.tex;echo) >> ${_G[header]}
+else
+	(cat fix-heavenlyclouds.tex |sed 's/LiSu/FandolHei/g';echo) >> ${_G[header]}
+fi
