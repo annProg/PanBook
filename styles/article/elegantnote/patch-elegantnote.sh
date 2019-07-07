ffa53f5af27bea453477ab04efbef7e8da6ef3f7
annProg <tecbbs@qq.com>
2019-07-08 02:16:23 +0800

    fix: elegantnote and elegantpaper amsmath mathspec bug

--- styles/article/elegantnote/patch-elegantnote.sh
+++ styles/article/elegantnote/patch-elegantnote.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-ext_wrap tex
-#ext_theorem tex
 # 默认模板需要禁用unicode-math. unicode-math和newtxmath不兼容
 # https://github.com/ElegantLaTeX/ElegantBook/issues/9
 _V[documentclass]="elegantnote"
 _P[standalone]=""
 _V[mathspec]="true"
+
+# amsmath and mathspec bug: 
+# https://tex.stackexchange.com/questions/85696/what-causes-this-strange-interaction-between-glossaries-and-amsmath
+FIX=${_G[build]}/fix-elegantnote.tex
+cat > $FIX <<EOF
+\makeatletter % undo the wrong changes made by mathspec
+\let\RequirePackage\original@RequirePackage
+\let\usepackage\RequirePackage
+\makeatother
+EOF
+
+writeHeader $FIX
+
+# wrap 写入的header需要放到 $FIX 后面（修复amsmath and mathspec bug）
+ext_wrap tex
+#ext_theorem tex
--- styles/article/elegantpaper/patch-elegantpaper.sh
+++ styles/article/elegantpaper/patch-elegantpaper.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 启用扩展
-ext_wrap tex
-#ext_theorem tex
 _P[standalone]=""
 
 # 默认模板需要禁用unicode-math. unicode-math和newtxmath不兼容
@@ -11,3 +8,20 @@ _V[mathspec]="true"
 _V[documentclass]="elegantpaper"
 getArrayVar _V classoption "cn"
 unset _P[toc]
+
+
+# amsmath and mathspec bug: 
+# https://tex.stackexchange.com/questions/85696/what-causes-this-strange-interaction-between-glossaries-and-amsmath
+FIX=${_G[build]}/fix-elegantpaper.tex
+cat > $FIX <<EOF
+\makeatletter % undo the wrong changes made by mathspec
+\let\RequirePackage\original@RequirePackage
+\let\usepackage\RequirePackage
+\makeatother
+EOF
+
+writeHeader $FIX
+
+# wrap 写入的header需要放到 $FIX 后面（修复amsmath and mathspec bug）
+ext_wrap tex
+#ext_theorem tex
