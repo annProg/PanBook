2f8f179f00e16c084e49427e4b24fc56d9278dea
annprog <tecbbs@qq.com>
2019-07-19 13:10:24 +0800

    fix: cv tmr and resume conflict

--- styles/cv/resume/patch-resume.sh
+++ styles/cv/resume/patch-resume.sh
@@ -2,4 +2,7 @@
 
 _G[ext-fontawesome5]=false
 
+# ext wrap定义的introduction和problemset环境和resume冲突
+unset _G[ext-wrap-introduction]
+unset _G[ext-wrap-problemset]
 _info "this is RESUME cv template"
--- styles/cv/tmr/patch-tmr.sh
+++ styles/cv/tmr/patch-tmr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 _G[ext-fontawesome5]=false
+# ext coverletter 冲突
+_G[ext-coverletter]=false
 _info "this is TMR cv template"
