501e1324409e42b2134de5b8732cdb8b8afd98a2
annProg <tecbbs@qq.com>
2019-08-10 20:40:29 +0800

    fix: ext listings 支持设置style(noframe and mdframe). fix 字体

--- extensions/listings/listings.sh
+++ extensions/listings/listings.sh
@@ -2,13 +2,21 @@ regext listings
 # 通用扩展
 getArrayVar _G "ext-listings" true
 getArrayVar _G "ext-listings-lstset" "$SCRIPTDIR/${_G[extdir]}/listings/listings-set.tex"
-getArrayVar _G "ext-listings-highlight" "$SCRIPTDIR/${_G[extdir]}/listings/highlight.tex"
+getArrayVar _G "ext-listings-style" "noframe"
 
 function listings() {
 	for i in `echo ${!_P[@]}`;do
 		[ $i == "listings" ] && echo true && return 0
 	done
 	echo false
+}
+
+function setStyle() {
+	if [ "${_G[ext-listings-set]}"x != ""x ];then
+		echo ${_G[ext-listings-set]}
+	else
+		echo $SCRIPTDIR/${_G[extdir]}/listings/${_G[ext-listings-style]}.tex
+	fi
 }
 
 function ext_listings() {
@@ -21,7 +29,8 @@ function ext_listings() {
 
 		extStat listings done
 	else
-		writeHeader ${_G[ext-listings-highlight]}
+		stylePath=`setStyle`
+		writeHeader $stylePath
 		extStat listings skip
 	fi
 }
@@ -29,5 +38,7 @@ function ext_listings() {
 function ext_listings_help() {
 	echo -e "\t-G ext-listings:<true|false>                        enable listings(default true)"
 	echo -e "\t-G ext-listings-lstset:<custom listings set file>   change listings set file"
+	echo -e "\t-G ext-listings-set:<custom set file>               change highlight set file"
+	echo -e "\t-G ext-listings-style:<mdframe|noframe>             highlight style.(mdframe or noframe)"
 	exit 0
 }
